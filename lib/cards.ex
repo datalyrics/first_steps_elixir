b8ae871f33195731fdb41df28fc6bd01434f4d54
datalyrics <nil@datalyrics.de>
2025-07-20 19:44:40 +0200

    add save and load function

--- lib/cards.ex
+++ lib/cards.ex
@@ -5,7 +5,7 @@ defmodule Cards do
     suits  = ["Hearts", "Diamonds", "Clubs", "Spades"]
 
     for suit <- suits, value <- values do
-      "#{value} of #{suit}"
+      "#{value} of #{suit}" 
     end
   end
 
@@ -16,5 +16,25 @@ defmodule Cards do
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
+
+  def deal(deck, hand_size) do
+    {hand, rest_of_deck} = Enum.split(deck, hand_size)
+
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} ->
+        :erlang.binary_to_term(binary)
+      {:error, _reason} ->
+        "File not found"
+    end
+  end
+
 
 end
